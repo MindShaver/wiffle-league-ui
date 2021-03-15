92c3740291b1e5b87a04a5bbc2419dc360cc539b
Robert Livingston <rll@bu.edu>
2021-03-15 12:47:17 -0500

    Add image

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import WiffleImage from "./images/wiffleLeague.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={WiffleImage} className="App-logo" alt="logo" />
+        <p>Check back for more things - like being able to log in!</p>
       </header>
     </div>
   );
